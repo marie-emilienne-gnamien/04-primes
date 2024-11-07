5a79bd2985918c1d51a2911c695f9458f3837165
marie-emilienne-gnamien <marie-emilienne.gnamien@edu.esiee.fr>
2024-11-07 09:04:45 +0000

    Exercice Nombres Premiers

--- main.py
+++ main.py
@@ -1,21 +1,47 @@
-from math import sqrt
+####from math import sqrt
 
 #### Fonction secondaire
+
+"""
+author: marie-emilienne.gnamien@edu.esiee.fr
+
+"""
 
 
 def isprime(p):
 
-    # votre code ici
+    """
+    Determine si p est premier
 
-    pass
+    Args:
+        p: valeur entiere positive
+    
+    Returns:
+        isprime(p) : True, False
+    """
+
+    premier = True
+    if p == 1:
+        return False
+    for i in range(2,p):
+        if p%i == 0:
+            premier = False
+            break
+    return premier
 
 #### Fonction principale
 
 
 def main():
+    """
+    Appelle la fonction secondaire et affiche 
+    les nombres premiers entre 0 et 99
 
+    """
     # vos appels à la fonction secondaire ici
-
+    print(isprime(2))
+    print(isprime(4))
+    print(isprime(20))
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
